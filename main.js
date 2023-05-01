92dbb0781b3a61c3030354b652563f4492e7d9bb
Drakonz <124328116+DrakonZZZ@users.noreply.github.com>
2023-05-01 21:42:45 +0530

    add ui element

--- main.js
+++ main.js
@@ -1,18 +1,25 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('node:path');
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 400,
+    width: 500,
     height: 600,
     webPreferences: {
-      preload: '',
+      preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true,
     },
     resizable: false,
+    titleBarStyle: 'hidden',
+    titleBarOverlay: {
+      color: '#252729',
+      symbolColor: '#FF5733',
+      height: 32,
+    },
   });
   win.setMenuBarVisibility(false);
   win.loadFile(path.join(__dirname, 'src', 'index.html'));
+  win.setBackgroundColor('#252729');
 };
 
 app.whenReady().then(() => {
